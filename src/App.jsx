98fb4cd9b355a743a7921bdb296a8276aec2c334
Ahmed Missouri <ahmedmissouri@icloud.com>
2023-02-15 22:55:35 +0000

    added search bar, type selector and search button with useState handling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,48 @@
+import { useState } from "react";
 import './App.css'
 
 function App() {
+  const [searchText, setSearchText] = useState("");
+
+  const onChange = (e) => {
+    setSearchText(e.target.value);
+  };
 
   return (
     <div className="App">
+      <div className="search">
+        <input
+          type="text"
+          id="searchTextarea"
+          onChange={onChange}
+          value={searchText}
+          placeholder="Search..."
+          required
+        />
+
+        <select id="type-selector">
+          <option value="multi">All</option>
+          <option value="movie">Movies</option>
+          <option value="tv">Tv Shows</option>
+          <option value="person">People</option>
+        </select>
+
+        <button
+          onClick={() => {
+            if (searchText.length <= 0) {
+              const textareaSearch = document.getElementById("searchTextarea");
+              textareaSearch.classList.add("error");
+              setTimeout(function () {
+                textareaSearch.classList.remove("error");
+              }, 300);
+            } else {
+              autoComplete("search", `?query=${searchText}`, selected.value);
+            }
+          }}
+        >
+          Search
+        </button>
+      </div>
     </div>
   )
 }
